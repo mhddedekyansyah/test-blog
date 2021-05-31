b31b1b5d6ba2c2fa822e988015a7b92476d8af78
Mhd Dedek Yansyah <dedek2203@gmail.com>
2021-05-31 13:53:10 +0700

    menambahkan style di inde.css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 export default function Home() {
+  let [blogs, setBlogs] = useState([
+    { id: 1, title: 'Jancook', body: 'jancook kocok kocok', author: 'bangal' },
+    { id: 2, title: 'Asuuu', body: 'Asuu coklat susu', author: 'benga' },
+    { id: 3, title: 'fucking mother ', body: 'yes no yes no ahh ahh', author: 'pornhub' },
+  ]);
+
   return (
     <div className="home">
-      <h2>HomePage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>Title is {blog.title}</h2>
+          <small>Author is {blog.author}</small>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
